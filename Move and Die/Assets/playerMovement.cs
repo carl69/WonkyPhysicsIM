0f889be88c36a2e5813e9cb37a81a712806f21ee
Carl Christian Von Munthe af Morgenstierne <carlmorgenstierne@yahoo.com>
2019-04-02 13:35:21 +0200

    walking player

--- Move and Die/Assets/playerMovement.cs
+++ Move and Die/Assets/playerMovement.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    // JUMPING
     [Header("Jump Settings")]
     public float JumpSpeed;
     public float JumpHight;
     float curJumpHight;
+    public float fallSpeed = 2;
+    float curFallSpeed = 0;
+    public LayerMask JumpableLayers;
+    // WALKING
+    [Header("Walking Settings")]
+    public float WalkingSpeed = 10;
 
 
     //Refrences
@@ -17,31 +24,72 @@ public class playerMovement : MonoBehaviour
     void Start()
     {
         RB = GetComponent<Rigidbody>();
+        curFallSpeed = fallSpeed;// set the fall speed on the start incase the player starts in the air
+    }
+
+    private void FixedUpdate()
+    {
+        //Walking
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            float inputHorizontal = Input.GetAxis("Horizontal");
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right * inputHorizontal), out hit, 0.5f, JumpableLayers))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right * inputHorizontal), Color.yellow); // shows Debug
+                print("WALLS");
+            }
+            else
+            {
+                //RB.AddForce(Vector3.right * WalkingSpeed * inputHorizontal * Time.deltaTime,ForceMode.Impulse);
+                transform.position += Vector3.right * WalkingSpeed * inputHorizontal * Time.deltaTime;
+            }
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+
+        //JUMPING 
         if (Input.GetButtonDown("Jump"))
         {
-            //setting maxHight
-            curJumpHight = JumpHight + transform.position.y;
+            // check if you are grounded
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 1, JumpableLayers)){
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow); // shows Debug
 
-            JumpTakeOff();
+                //setting maxHight
+                curJumpHight = JumpHight + transform.position.y;
 
-            StartCoroutine("Jumping");
+                JumpTakeOff();
+
+                StartCoroutine("Jumping");
+
+                curFallSpeed = 0;
+
+            }
         }
-
         if (Input.GetButtonUp("Jump"))
         {
             curJumpHight = 0;
         }
+
+        // FakeGravity
+        RB.velocity -= new Vector3(0, Time.deltaTime * curFallSpeed, 0); //Placeholder
+
     }
 
+    // the jump start
     void JumpTakeOff()
     {
         RB.AddForce(Vector3.up * JumpSpeed,ForceMode.Impulse);
     }
+
 
     IEnumerator Jumping()
     {
@@ -50,6 +98,9 @@ public class playerMovement : MonoBehaviour
         // checks if the player are moving down and stopping the jump here
         if (RB.velocity.y <= 0)
         {
+            print("Happen");
+            curFallSpeed = fallSpeed;
+
             yield return null;
         }
 
@@ -61,8 +112,7 @@ public class playerMovement : MonoBehaviour
         }
         else
         {
-            Debug.Log(curJumpHight);
-            Debug.Log(transform.position.y);
+            curFallSpeed = fallSpeed;
 
             RB.velocity = new Vector3(
                 RB.velocity.x,
