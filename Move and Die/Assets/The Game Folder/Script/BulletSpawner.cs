0b57e2928f593e9c2e6d2d934a1046cf546ea5b9
Carl Christian Von Munthe af Morgenstierne <carlmorgenstierne@yahoo.com>
2019-04-25 15:49:20 +0200

    here you go

--- Move and Die/Assets/The Game Folder/Script/BulletSpawner.cs
+++ Move and Die/Assets/The Game Folder/Script/BulletSpawner.cs
@@ -15,7 +15,7 @@ public class BulletSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("SpawnBullet"))
+        if (Input.GetButtonDown("SpawnBullet") || Input.GetButtonDown("crouch"))
         {
 
 
