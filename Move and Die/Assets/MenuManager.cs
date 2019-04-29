41335dc642365f684e598c2ef60029d1368b7bb4
Carl Christian Von Munthe af Morgenstierne <carlmorgenstierne@yahoo.com>
2019-04-30 00:44:11 +0200

    STOP YELLING AFTER 23.00!!!!

---
+++ Move and Die/Assets/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    
+
+
+
+   public void EzLevel()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void HardLevel1()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+    public void HardLevel2()
+    {
+        SceneManager.LoadScene(3);
+
+    }
+}
--- Move and Die/Assets/The Game Folder/Script/EzEndGameButton.cs
+++ Move and Die/Assets/The Game Folder/Script/EzEndGameButton.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EzEndGameButton : MonoBehaviour
 {
-
+    public bool MainMenu = false; 
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetButtonDown("Cancel"))
         {
-            Application.Quit();
+            if (MainMenu)
+            {
+                Application.Quit();
+
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+            
         }
     }
 }
