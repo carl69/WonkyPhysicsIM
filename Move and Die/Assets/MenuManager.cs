ddc3014c89d69a8168d82eae00e42e413d60b903
Cookies <julies.cookies13@outlook.com>
2019-05-25 12:25:28 +0200

    Revert "fh"
    This reverts commit 1fb77ac19f42358f2f398a0c3a4908f34800b5f6, reversing
    changes made to 5ff50584ab3d0c575836b57d0e1bc830eed7e9cb.

--- Move and Die/Assets/MenuManager.cs
+++ Move and Die/Assets/MenuManager.cs
@@ -12,20 +12,20 @@ public class MenuManager : MonoBehaviour
     }
 
 
-   public void Tutorial()
+   public void EzLevel()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void HardLevel1()
     {
         SceneManager.LoadScene(2);
 
     }
-
-    public void SkipTutorial()
+    public void HardLevel2()
     {
         SceneManager.LoadScene(3);
-
-    }
-    public void LevelSelect()
-    {
-        SceneManager.LoadScene(1);
 
     }
 }
