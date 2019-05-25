a0812a7f6f8855c68625a714dd5430fd3fd4d9b6
Carl Christian Von Munthe af Morgenstierne <carlmorgenstierne@yahoo.com>
2019-05-25 15:55:01 +0200

    have fun

---
+++ Move and Die/Assets/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+
+public class AudioManager : MonoBehaviour
+{
+    [HideInInspector]
+    public float Bass;
+    void FixedUpdate()
+    {
+        float[] spectrum = new float[256];
+
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
+        Bass = spectrum[1];
+    }
+}
---
+++ Move and Die/Assets/BulletAudioEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAudioEffect : MonoBehaviour
+{
+    AudioManager AudioManager;
+    float Bass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Bass = AudioManager.Bass * 5 + 1;
+        transform.localScale = new Vector3(Bass, Bass, Bass);
+
+    }
+}
