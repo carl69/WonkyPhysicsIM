781d1cc8f4d8ba44f32c519f31a59a0c1514f827
Carl Christian Von Munthe af Morgenstierne <carlmorgenstierne@yahoo.com>
2019-06-05 14:33:15 +0200

    Next level canvas and sound can be placed in now

--- Move and Die/Assets/The Game Folder/Script/NextLevel.cs
+++ Move and Die/Assets/The Game Folder/Script/NextLevel.cs
@@ -7,26 +7,35 @@ public class NextLevel : MonoBehaviour
 {
     public bool LastLevel = false;
     GameManagerScript GMS;
+    AudioSource AudioS;
+    bool won = false;
+    public GameObject LevelCanvas;
     private void Start()
     {
+        AudioS = GetComponent<AudioSource>();
         GMS = GameObject.FindGameObjectWithTag("GM").GetComponent<GameManagerScript>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && won == false)
         {
+            won = true;
             GMS.GameWon();
+            AudioS.Play();
+            LevelCanvas.SetActive(true);
+        }
+    }
+    public void LevelSelect()
+    {
+        if (!LastLevel)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
-            if (!LastLevel)
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
-            }
-            else
-            {
-                SceneManager.LoadScene(0);
-            }
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
         }
     }
 }
